7781c9a7769913bea32713cbee28b23fe94d4425
HanselVinicius <hanelvinicius@gmail.com>
2023-12-21 16:49:42 -0300

    initial components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonalInfoComponent } from './components/personal_info/personal-info/personal-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo:"/personal_info",
+    pathMatch:"full"
+  },
+  {
+    path: "personal_info",
+    component: PersonalInfoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './components/header/header.component';
+import { PersonalInfoComponent } from './components/personal_info/personal-info/personal-info.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PersonalInfoComponent
   ],
   imports: [
     MatToolbarModule,
