823a5e6bdd5277121caa2c2db8d4b17681fdcf83
HanselVinicius <hanelvinicius@gmail.com>
2023-12-22 15:37:26 -0300

    some configurations

---
+++ src/app/services/CurriculumApiClient.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { AxiosInstance } from "axios";
+import axios from "axios";
+
+@Injectable({
+	providedIn: "root"
+})
+export class CurriculumApiClient {
+    
+    private readonly _baseUrl: string = "todo";
+    private axiosClient:AxiosInstance;
+    
+    constructor() {
+    this.axiosClient = axios.create({
+        baseURL: this._baseUrl,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    }
+
+
+    public getClient(){
+        return this.axiosClient;
+    }
+
+    public getClienWithAuth(token:string){
+        this.axiosClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        return this.axiosClient;
+    }
+}
